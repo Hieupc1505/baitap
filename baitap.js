c6435ad48f8ddd409375effe9e2dc2d24e7baabf
unknown <vuhieukenny2@gmail.com>
2020-12-07 15:20:16 +0700

    Add file dog.js

--- baitap.js
+++ baitap.js
@@ -3,3 +3,14 @@ var Mouse = {
 	lastname : "bin", 
 	firstname : "hoang"
 }; 
+
+
+var cat = { 
+	name : 'tom', 
+	lastname: 'bo', 
+	firstname: 'tubo'
+	sayHi(){
+		console.log(`Hello ${this.name + this.lastname}`); 
+
+	}
+}
